d2a7f1c730ed0e07fd73cdf942a01644316eb3c9
garysheh <jiayaoshe@gmail.com>
2022-03-12 22:24:23 -0500

    updated on 12 March, added Townscene baked env and char mov

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
+public class MouseManager : MonoBehaviour
+{
+    RaycastHit hitInfo;
+    public EventVector3 OnMouseClicked;
+    void Update() {
+        SetCursorTexture();
+        MouseControl();
+    }
+
+    void SetCursorTexture()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hitInfo))
+        {
+            // Changing mouse texture
+        }
+    }
+
+    void MouseControl() {
+        if(Input.GetMouseButton(0) && hitInfo.collider != null)
+        {
+            if(hitInfo.collider.gameObject.CompareTag("Ground"))
+            {
+                OnMouseClicked?.Invoke(hitInfo.point);
+            }
+        }
+    }
+}
