bf87485ec827ad712c0ce1840c2cfb2b3255da10
garysheh <jiayaoshe@gmail.com>
2022-04-30 21:53:05 -0400

    fixed test

--- Assets/Scripts/Character Stats/ScriptableObject/CharacterData_SO.cs
+++ Assets/Scripts/Character Stats/ScriptableObject/CharacterData_SO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,4 +13,37 @@ public class CharacterData_SO : ScriptableObject
     public int currentMana;
     public int baseDefence;
     public int currentDefence;
+
+    [Header("Level")]
+    public int curlevel; // current level;
+    public int topLevel; // maximum level;
+    public int expBase; // exp base
+    public int curExp; // current exp
+    public float levelBuff;
+
+    public float levelMultiplier
+    {
+        get
+        {
+            return (curlevel - 1) * levelBuff + 1;
+        }
+    }
+
+    public void getExp(int exp)
+    {
+        curExp += exp;
+
+        if (curExp >= expBase)
+        {
+            leveling();
+        }
+    }
+
+    void leveling()
+    {
+        curlevel = Mathf.Clamp(curlevel + 1, 0, topLevel); // to distingush between level 0 to the maximum level;
+        expBase += (int)(expBase * levelMultiplier);
+        maxHealth = (int)(maxHealth * levelMultiplier);
+
+    }
 }
